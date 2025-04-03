c83dfcff6cdd85c512258d3f8fae17c78357a2d2
arsh <aa5755@srmist.edu.in>
2025-04-03 16:33:08 +0530

    fix workflow

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send('CICD Pipeline is working');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
